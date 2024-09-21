2656e367bab64b333d60cf5b9a7f76bd6179dc66
aadityapattabhiraman <aadityapattabhiraman47@proton.me>
2024-09-21 09:51:48 +0000

    Modified download file for imagewoof

--- Image/Classification/mrk1.py
+++ Image/Classification/mrk1.py
@@ -1,8 +1,5 @@
 from datasets import load_dataset
 
-# Specify the dataset name and cache directory
-dataset_name = "glue/mnli"
-cache_dir = "../Datasets/"
 
-# Load the dataset
+cache_dir = "../Datasets/"
 dataset = load_dataset("frgfm/imagewoof", "full_size", cache_dir=cache_dir)
