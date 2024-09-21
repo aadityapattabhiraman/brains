fe0e1d94f757c9e65c03e1ccb4d951c988fe54a6
aadityapattabhiraman <aadityapattabhiraman47@proton.me>
2024-09-21 09:56:03 +0000

    Updated file to display first image

--- Image/Classification/mrk1.py
+++ Image/Classification/mrk1.py
@@ -1,5 +1,11 @@
 from datasets import load_dataset
+import matplotlib.pyplot as plt
 
 
 cache_dir = "../Datasets/"
 dataset = load_dataset("frgfm/imagewoof", "full_size", cache_dir=cache_dir)
+
+first_image = dataset["train"][0]["image"]
+
+plt.imshow(first_image)
+plt.show()
