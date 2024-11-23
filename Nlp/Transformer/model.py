c42ca9bc54283b8aaf342f479aa51496d66cb720
aadityapattabhiraman <154038229+aadityapattabhiraman@users.noreply.github.com>
2024-11-23 12:21:40 +0000

    Layer Normalization

--- Nlp/Transformer/model.py
+++ Nlp/Transformer/model.py
@@ -20,6 +20,7 @@ class InputEmbeddings(nn.Module):
         is the size of the vector whose value given in the paper is 512,
         vocab_size defines the size of the vocabulary.
         """
+
         super().__init__()
         self.d_model = d_model
         self.vocab_size = vocab_size
@@ -30,6 +31,7 @@ class InputEmbeddings(nn.Module):
         The embedding is multiplied by a factor of sqrt of the d_model
         as specified by the paper.
         """
+
         return self.embedding(x) * math.sqrt(self.d_model)
 
 
@@ -38,10 +40,12 @@ class PositionalEncoding(nn.Module):
     Given a set of input tokens, add positional encodings to it and
     return the output. Created only once, not a parameter.
     """
+
     def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
         """
         Takes 3 arguments: d_model(int), vocab_size(int), dropout(int.)
         """
+
         super().__init__()
         self.d_model = d_model
         self.seq_len = seq_len
@@ -60,7 +64,7 @@ class PositionalEncoding(nn.Module):
 
         # Increase the dimension by 1 because data is sent in the form
         # of batches (1, seq_len, d_model)
-        pe = pe.unsqueeze(0)
+        positional_encoding = positional_encoding.unsqueeze(0)
         # Stores pe in model but not as a parameter, but allows to save
         # it when saving a model
         self.register_buffer("positional_encoding", positional_encoding)
@@ -69,5 +73,55 @@ class PositionalEncoding(nn.Module):
         """
         Add positional encoding to Input Embedding
         """
+
         x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False)
         return self.dropout(x)
+
+
+class LayerNormalization(nn.Module):
+    """
+    Performs Layer Normalization
+    """
+
+    def __init__(self, eps: float = 10**-6):
+        """
+        Takes 1 argument: eps.
+        """
+
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1))
+        self.bias = nn.Parameter(torch.zeros(1))
+
+    def forward(self, x: torch.Tensor):
+        """
+        (Data point - mean) / (standard_deviation + epsilon) * alpha
+        + bias
+        """
+
+        mean = x.mean(dim=-1, keepdim=True)
+        std = x.std(dim=-1, keepdim=True)
+
+        return self.alpha * (x - mean) / (std + self.eps) + self.bias
+
+
+class FeedForwardBlock(nn.Module):
+    """
+    Contains of 2 linear layers, a relu and a dropout in between them.
+    """
+
+    def __init__(self, d_model: int, linear_middle: int, dropout: float):
+        """
+        Takes 3 arguments: d_model(int), linear_middle(int),
+        dropout(int).
+        """
+
+        super().__init__()
+        self.feed_forward = nn.Sequential(nn.Linear(d_model, linear_middle),
+                                          nn.ReLU(),
+                                          nn.Dropout(),
+                                          nn.Linear(linear_middle, d_model),
+        )
+
+    def forward(self, x: torch.Tensor):
+        return self.feed_forward(x)
